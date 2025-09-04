ba518bb0953206313dadb85eb8075dd79374c5c2
Evie <kccmewal@yahoo.com>
2025-09-04 22:47:58 +0800

    第一次上傳專案

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+
+<template>
+  <nav>
+    <router-link to="/">首頁</router-link> |
+    <router-link to="/about">關於</router-link>
+  </nav>
+  <router-view></router-view>
+</template>
+
+<script setup>
+</script>
+
+
+<style scoped>
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,7 @@
+<template>
+  <h1 class="font-huninn text-brown-400">Home</h1>
+</template>
+<script setup>
+</script>
+<style scoped>
+</style>
